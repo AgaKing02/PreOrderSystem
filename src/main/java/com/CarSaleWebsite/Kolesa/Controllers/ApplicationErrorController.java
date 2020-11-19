e6c8e1e9fc8888e63d42ecd6094229b80d0e85ac
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2020-11-19 15:44:52 +0600

    TwilioConfigs

--- src/main/java/com/CarSaleWebsite/Kolesa/Controllers/ApplicationErrorController.java
+++ src/main/java/com/CarSaleWebsite/Kolesa/Controllers/ApplicationErrorController.java
@@ -8,31 +8,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+//@Controller
 public class ApplicationErrorController implements ErrorController {
     public ApplicationErrorController(){}
     
-    @GetMapping(value = "/error")
-    public String handleError(HttpServletRequest request) {
-
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if (status != null) {
-
-            int statusCode = Integer.parseInt(status.toString());
-
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error-404";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500";
-            }
-            else if(statusCode==HttpStatus.FORBIDDEN.value()){
-                return "error-403";
-            }
-        }
-        return "error";
-    }
+//    @GetMapping(value = "/error")
+//    public String handleError(HttpServletRequest request) {
+//
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//
+//        if (status != null) {
+//
+//            int statusCode = Integer.parseInt(status.toString());
+//
+//            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+//                return "error-404";
+//            }
+//            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+//                return "error-500";
+//            }
+//            else if(statusCode==HttpStatus.FORBIDDEN.value()){
+//                return "error-403";
+//            }
+//        }
+//        return "error";
+//    }
     @Override
     public String getErrorPath() {
         return null;
